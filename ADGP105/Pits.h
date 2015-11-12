f1a31336ed674b028d5d82c7df60ec11319b541c
Bernard Castello <bernard.castello@yahoo.com>
2015-11-12 17:01:53 -0600

    Revisions for 11/12/2015

--- ADGP105/Pits.h
+++ ADGP105/Pits.h
@@ -4,12 +4,22 @@ struct Pit
 {
 	int posX;
 	int posY;
+	bool playerFalls;
 
 	void SpawnPit();
+	void PlayerFalls(Player);
 };
 
 void Pit::SpawnPit()
 {
 	posX = 3;
 	posY = 2;
+}
+
+void Pit::PlayerFalls(Player pl)
+{
+	if (pl.currentPosX && pl.currentPosY == posX && posY)
+	{
+		playerFalls = true;
+	}
 }
